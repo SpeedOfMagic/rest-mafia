2e26b46a3e3a0e41dd8c2fb78475f2c21568a88d
SpeedOfMagic <davasilev_4@edu.hse.ru>
2022-04-09 11:17:37 +0300

    Added header

--- rest/app.py
+++ rest/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, Markup
 
 
 app = Flask(__name__)
@@ -14,9 +14,15 @@ def all_profiles():
     return open('templates/all_profiles.html').read()
 
 
-@app.route('/register')
-def register():
+@app.route('/register', methods=['GET'])
+def register_get():
     return open('templates/register.html').read()
+
+
+@app.route('/register', methods=['POST'])
+def register_post():
+    print('POSTED!')
+    return open('templates/registration_complete.html').read()
 
 
 @app.route('/authorize')
@@ -24,15 +30,20 @@ def authorize():
     return open('templates/authorize.html').read()
 
 
-@app.route('/favicon.ico')
-def fav():
-    return open('templates/favicon.ico', 'rb').read()
-
-
 @app.route('/profile/<string:login>')
 def profile(login):
     print(login)
     return open('templates/profile.html').read()
+
+
+@app.route('/ico.png')
+def ico():
+    return open('templates/ico.png', 'rb').read()
+
+
+@app.route('/favicon.ico')
+def fav():
+    return open('templates/favicon.ico', 'rb').read()
 
 
 if __name__ == '__main__':
